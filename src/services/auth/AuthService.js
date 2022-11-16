1706ee508e7564c3baa8184a046561050d58d5d3
Kareem Hesham <karimdeveloper@outlook.com>
2022-11-16 15:48:30 +0200

    implemented auth service methods

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -11,6 +11,7 @@ import { auth } from "../../config";
 const googleProvider = new GoogleAuthProvider();
 
 class AuthService {
+  // Actions available in auth service
   Action = {
     SignIn: "login",
     Register: "register",
@@ -18,19 +19,26 @@ class AuthService {
     SignOut: "signout",
   };
 
+  // Wrapper method for accessing auth services
   authenticate = async (action, provider, payload) => {
+    if (
+      (action === this.Action.SignIn || action === this.Action.Register) &&
+      provider === "google"
+    ) {
+      this.googleSignIn(payload);
+    }
     switch (action) {
       case this.Action.SignIn:
-        this.signIn(provider, payload);
+        this.signIn(payload);
         break;
       case this.Action.Register:
-        this.register(provider, payload);
+        this.register(payload);
         break;
       case this.Action.Reset:
         this.reset(payload);
         break;
       case this.Action.SignOut:
-        this.signOut(auth);
+        this.signOut();
         break;
       default:
         console.log("Invalid Action");
@@ -38,33 +46,44 @@ class AuthService {
     }
   };
 
-  signIn = async (provider, payload) => {
-    switch (provider) {
-      case "local":
-        try {
-          await signInWithEmailAndPassword(
-            auth,
-            payload.email,
-            payload.password
-          );
-        } catch (err) {
-          console.log(err);
-        }
-        break;
-      case "google":
-        try {
-          await signInWithPopup(auth, googleProvider);
-        } catch (err) {
-          console.log(err);
-        }
-        break;
-      default:
-        console.log("Invalid Auth Provided");
-        break;
+  googleSignIn = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.log(err);
     }
   };
 
-  register = async (provider, payload) => {};
-  reset = async (payload) => {};
-  signOut = async (payload) => {};
+  signIn = async (payload) => {
+    try {
+      await signInWithEmailAndPassword(auth, payload.email, payload.password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  register = async (payload) => {
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        payload.email,
+        payload.password
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  reset = async (payload) => {
+    try {
+      await sendPasswordResetEmail(auth, payload.email);
+      console.log("Password reset sent...");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  signOut = async () => {
+    signOut(auth);
+  };
 }
