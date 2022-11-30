fb3b850e1daa0a278475f371ef9a1303ddfa83f2
Kareem Hesham <karimdeveloper@outlook.com>
2022-12-01 00:04:59 +0200

    clean up - [removed unnecessary import]

--- src/features/index.js
+++ src/features/index.js
@@ -1,5 +1,3 @@
-import Editor from "./editor/Editor";
-
 export { default as Landing } from "./landing/Landing";
 export { default as Register } from "./register/Register";
 export { default as Home } from "./home/Home";
