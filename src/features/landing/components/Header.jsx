faabd406a6c097f32141678b31f8cf92ab58120c
Kareem Hesham <karimdeveloper@outlook.com>
2022-12-05 19:24:52 +0200

    some changes - [add logo to Header component]

--- src/features/landing/components/Header.jsx
+++ src/features/landing/components/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import { Button, Stack } from "@mui/material";
+import logo from "../../../assets/arab-logo.png";
 
 const Header = () => {
   return (
     <Stack flex={1} alignItems="center" justifyContent="center" py={1}>
+      <img src={logo} alt="" width={200} height={150} />
+      <Stack height={20} />
       <Link to="/register">
         <Button variant="contained" size="large" sx={{ fontSize: 30 }}>
           ابدأ الآن
