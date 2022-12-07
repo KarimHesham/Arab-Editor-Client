4dce828c5d934b620974db6b0dca9909b95327cf
Kareem Hesham <karimdeveloper@outlook.com>
2022-12-07 19:26:08 +0200

    save html, css to page data onStore

--- src/features/grapes/config/storageManager.js
+++ src/features/grapes/config/storageManager.js
@@ -17,8 +17,18 @@ export const storageManager = (pageId) => {
       remote: {
         urlLoad: `https://firestore.googleapis.com/v1/projects/arab-editor/databases/(default)/documents/pages/${pageId}?mask.fieldPaths=content`,
 
-        onStore: (data) => {
-          PagesService.update("id", pageId, { content: data }, "", "pages")
+        onStore: (data, editor) => {
+          const htmlCode = editor.getHtml();
+          const cssCode = editor.getCss();
+          console.log("Editor HTML: ", editor.getHtml());
+          console.log("Editor CSS: ", editor.getCss());
+          PagesService.update(
+            "id",
+            pageId,
+            { content: data, code: { html: htmlCode, css: cssCode } },
+            "",
+            "pages"
+          )
             .then((res) => console.log(res))
             .catch((err) => console.log(err));
         },
