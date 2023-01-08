3cef5b533895b8856cf0788dc45f6f1f3aee4c51
Kareem Hesham <karimdeveloper@outlook.com>
2023-01-08 19:30:27 +0200

    update output html head

--- src/features/output/Output.jsx
+++ src/features/output/Output.jsx
@@ -3,6 +3,11 @@ const Output = () => {
     <!DOCTYPE html>
     <html lang="en">
     <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Grapes Example</title>
+
     <style>* {
         box-sizing: border-box;
       }
